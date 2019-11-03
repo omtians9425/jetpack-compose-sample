6da68dec6da2f71b472f9aade7c60d73edd81bd6
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-11-03 20:46:25 +0900

    update gitignore

--- app/src/main/java/com/example/jetpack_compose_sample/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_sample/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
+import androidx.ui.core.dp
 import androidx.ui.core.setContent
+import androidx.ui.graphics.Color
+import androidx.ui.layout.Spacing
 import androidx.ui.material.MaterialTheme
+import androidx.ui.material.surface.Surface
 import androidx.ui.tooling.preview.Preview
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Call composable functions within setContent
         setContent {
             MaterialTheme {
                 Greeting("Android")
@@ -21,7 +26,9 @@ class MainActivity : AppCompatActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = Color.Yellow) {
+        Text(text = "Hello $name!", modifier = Spacing(24.dp))
+    }
 }
 
 @Preview
