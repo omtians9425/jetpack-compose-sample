b7940664454d6c9ef887c75c5fa2865b8085e6ad
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-03-25 22:34:44 +0900

    create @Model and implement recompose

--- app/src/main/java/com/example/jetpack_compose_sample/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_sample/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.jetpack_compose_sample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.Model
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
 import androidx.ui.graphics.Color
 import androidx.ui.layout.Column
 import androidx.ui.layout.LayoutPadding
+import androidx.ui.material.Button
 import androidx.ui.material.Divider
 import androidx.ui.material.MaterialTheme
 import androidx.ui.material.Surface
@@ -36,18 +38,33 @@ fun myApp(children: @Composable() () -> Unit) {
 }
 
 @Composable
-fun MyScreenContent(names: List<String> = listOf("Android", "there")) {
+fun MyScreenContent(
+    names: List<String> = listOf("Android", "there"),
+    counterState: CounterState = CounterState()
+) {
     Column {
         for (name in names) {
             Greeting(name = name)
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, height = 32.dp)
+        Counter(state = counterState)
     }
 }
 
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!", modifier = LayoutPadding(24.dp))
+}
+
+@Model
+class CounterState(var count: Int = 0)
+
+@Composable
+fun Counter(state: CounterState) {
+    Button(onClick = { state.count++ }) {
+        Text(text = "I 've been clicked ${state.count} times")
+    }
 }
 
 @Preview
