3a448073555df99a2e8ac7429acdfa7c1af4a5cb
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-03-25 22:12:00 +0900

    update versions

--- app/src/main/java/com/example/jetpack_compose_sample/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_sample/MainActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
 import androidx.ui.core.Text
-import androidx.ui.core.dp
 import androidx.ui.core.setContent
 import androidx.ui.graphics.Color
-import androidx.ui.layout.Spacing
+import androidx.ui.layout.LayoutPadding
 import androidx.ui.material.MaterialTheme
-import androidx.ui.material.surface.Surface
+import androidx.ui.material.Surface
 import androidx.ui.tooling.preview.Preview
+import androidx.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,13 +33,11 @@ fun myApp() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Spacing(24.dp))
+    Text(text = "Hello $name!", modifier = LayoutPadding(24.dp))
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
-    MaterialTheme {
-        Greeting("Android")
-    }
+    myApp()
 }
